fd7b3f95a129c80344e2d4f24224599646e67aff
Aleksei Piatachkov <pyatachkov.ao@gmail.com>
2023-04-03 13:02:34 +0500

    Added work2

---
+++ Work2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+/* Задача 27: Напишите программу, которая принимает на вход 
+число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write($"{number} -> ");
+Console.Write(GetSumArray(number));
+
+int GetSumArray(int number)
+{
+    int i = 0;
+    int sum = 0;
+    while (number > 10)
+    {
+        sum = number % 10 + sum;
+        number = number / 10;
+        i++;
+    }
+    sum = number + sum;
+    return sum;
+}
